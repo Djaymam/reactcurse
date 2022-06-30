4f32c6ac8a69d6799cc788c020ad48937d4b67aa
Jaylson Almeida <may2almeida@gmail.com>
2022-06-30 21:55:36 -0100

    update v.1
    and done for now

--- src/App.js
+++ src/App.js
@@ -12,9 +12,20 @@ function App() {
           <h2>FrontEnd Developer</h2>
           <h4>j.almeidacv@gmail.com</h4>
           <div className="btn-container">
-           <button className="btn1"><i class="material-symbols-outlined"></i>Email</button>
-           <button className="btn2"><i></i>LinkedIn</button>
+           <button className="btn1">Email</button>
+           <button className="btn2">LinkedIn</button>
           </div>
+          <main className="main-conteiner" >
+            <h2 >About</h2>
+            <p>I'm 29 years old trying my best to serve the world better, 
+              right now teaching myself to program, slow but steady steps 
+              to change my career and my life for the better.Already know 
+              about the basics of HTML, CSS, JAVASCRIPT, PYTHON, REACT, and SQL.
+              </p>
+              <h2>Interests</h2>
+              <p>Food,Technology,Gaming,Anime,Music,Internet Enthusiast</p>
+
+          </main>
         </div>
       
       
